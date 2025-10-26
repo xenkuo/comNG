5bfbb127925601cf8886f96f58a2b1952f98f590
xenkuo <xenkuo@163.com>
2025-10-26 12:08:52 +0800

    - Change folder name

--- src/fragment-loader.js
+++ src/fragment-loader.js
@@ -14,12 +14,12 @@ function loadFragment(placeholderId, fragmentFile) {
 window.addEventListener('DOMContentLoaded', function () {
   loadFragment('nav-area-placeholder', 'nav-area.html')
   const tabFragments = [
-    { id: 'general-tab-placeholder', file: 'tabs/general-tab.html' },
-    { id: 'transmit-tab-placeholder', file: 'tabs/transmit-tab.html' },
-    { id: 'advance-tab-placeholder', file: 'tabs/advance-tab.html' },
-    { id: 'fileops-tab-placeholder', file: 'tabs/fileops-tab.html' },
-    { id: 'chart-tab-placeholder', file: 'tabs/chart-tab.html' },
-    { id: 'about-tab-placeholder', file: 'tabs/about-tab.html' },
+    { id: 'general-tab-placeholder', file: 'tabs_html/general-tab.html' },
+    { id: 'transmit-tab-placeholder', file: 'tabs_html/transmit-tab.html' },
+    { id: 'advance-tab-placeholder', file: 'tabs_html/advance-tab.html' },
+    { id: 'fileops-tab-placeholder', file: 'tabs_html/fileops-tab.html' },
+    { id: 'chart-tab-placeholder', file: 'tabs_html/chart-tab.html' },
+    { id: 'about-tab-placeholder', file: 'tabs_html/about-tab.html' },
   ]
   tabFragments.forEach(function (tab) {
     loadFragment(tab.id, tab.file)
