1a1e58596f4c8f70b173724eefb5c77d457f1142
xenkuo <xenkuo@163.com>
2026-02-09 20:10:29 +0800

    Implem i18n for titles

--- src/modules/i18n.js
+++ src/modules/i18n.js
@@ -3,22 +3,28 @@ const { remote } = require('electron')
 const locale = remote.app.getLocale()
 console.log('Locale:', locale)
 
-const translations = {
+const titleI18n = {
   zh: {
     setting: '设置',
+    portSwitch: '串口开关',
+    cleanData: '清空数据',
+    autoRefresh: '自动刷新',
   },
   en: {
     setting: 'Settings',
+    portSwitch: 'Port Switch',
+    cleanData: 'Clean Data',
+    autoRefresh: 'Auto Refresh',
   },
 }
 
 function applyLanguage() {
   // 如果语言不存在，默认英文
   let lang = locale.split('-')[0]
-  if (!translations[lang]) lang = 'en'
+  if (!titleI18n[lang]) lang = 'en'
   document.querySelectorAll('[data-i18n-title]').forEach((el) => {
     const key = el.getAttribute('data-i18n-title')
-    el.title = translations[lang][key] || key
+    el.title = titleI18n[lang][key] || key
   })
 }
 
